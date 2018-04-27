36741c904d6f8607a37fad6d71e14b41bbb97778
贾伟康 <jiaweikang@jiaweikangdeimac.local>
2018-04-27 17:03:08 +0800

    666

---
+++ qqqq/qqqq/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  qqqq
+//
+//  Created by 贾伟康 on 2018/4/27.
+//  Copyright © 2018年 贾伟康. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("小迪好牛逼，肤白腿长，人美歌甜😄")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
